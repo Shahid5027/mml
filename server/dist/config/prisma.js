3e18c819f70d2cffec7f65ee77220705e73a3d5d
Shahid5027 <97589989+Shahid5027@users.noreply.github.com>
2026-05-18 14:15:27 +0530

    v0

--- server/dist/config/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-exports.prisma = new client_1.PrismaClient();
-// Handle graceful shutdown of Prisma connection
-process.on('beforeExit', async () => {
-    await exports.prisma.$disconnect();
-});
